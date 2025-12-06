4b1a9d70046ed2187f2acfe677abc5467108403a
Big-Stan <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2025-12-06 21:38:20 +0000

    Update Login.jsx

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://lecharlotlimo.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
