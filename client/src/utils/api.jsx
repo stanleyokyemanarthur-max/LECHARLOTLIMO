ad79bda343eed27670988c3b5cd5b1afa1f249f2
Big-Stan <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2025-12-06 21:56:02 +0000

    Update api.jsx

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base URL
+  baseURL: "https://lecharlotlimo.onrender.com/api", // backend base URL
 });
 
 // Attach token to requests if exists
