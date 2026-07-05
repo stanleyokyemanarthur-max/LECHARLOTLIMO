d42821ca5e7591abdbd2993ba9cf257cf5a68dc2
Stanley Essuman Arthur <244668394+stanleyokyemanarthur-max@users.noreply.github.com>
2026-07-05 19:01:10 +0000

    updated

--- server/config/paypal.js
+++ server/config/paypal.js
@@ -1,8 +1,8 @@
 import paypal from "@paypal/checkout-server-sdk";
 
 const environment =
-  process.env.PAYPAL_ENV === "live"
-    ? new paypal.core.LiveEnvironment(
+  process.env.PAYPAL_ENV === "sandbox"
+    ? new paypal.core.SandboxEnvironment(
         process.env.PAYPAL_CLIENT_ID,
         process.env.PAYPAL_CLIENT_SECRET
       )
